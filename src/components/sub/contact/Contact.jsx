6bbb4298d38f0980cc0cbdb38607f20d5d98e115
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-05 15:43:58 +0900

    마커 위치 수정

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -13,7 +13,7 @@ export default function Contact() {
 
 	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
 	const imageSize = new kakao.maps.Size(232, 99);
-	const imageOption = { offset: new kakao.maps.Point(0, 0) };
+	const imageOption = { offset: new kakao.maps.Point(116, 90) };
 	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
 	//마커 위치 인스턴스를 인술 전달해서 마커 출렷 인스턴스 객체를 생성
