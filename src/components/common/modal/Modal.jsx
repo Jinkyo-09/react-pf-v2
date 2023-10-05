4650f5cf9c9bc706f681ffdf107bf1bb6ecc8d78
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-05 13:39:20 +0900

    forwardRef 전단계로 모달 로직 되돌림

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -1,24 +1,12 @@
 import './Modal.scss';
-import { forwardRef, useState, useImperativeHandle } from 'react';
-//forwardRef : forwardRef((ref,props)=>{})
-//forwardRef 안쪽에서 ref로 연결한 요소를 역으로 부모 컴포넌트에 전달 가능 (자식 컴포넌트의 jsx 요소를 부모로 전달)
 
-const Modal = forwardRef(({ children }, ref) => {
-	const [Open, setOpen] = useState(false);
-	useImperativeHandle(ref, () => {
-		return { open: () => setOpen(true) };
-	});
-
+const Modal = ({ children, setIsModal }) => {
 	return (
-		<>
-			{Open && (
-				<aside className='modal'>
-					<div className='con'>{children}</div>
-					<span onClick={() => setOpen(false)}>close</span>
-				</aside>
-			)}
-		</>
+		<aside className='modal'>
+			<div className='con'>{children}</div>
+			<span onClick={() => setIsModal(false)}>close</span>
+		</aside>
 	);
-});
+};
 
 export default Modal;
--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,42 +1,37 @@
 import Layout from '../../common/layout/Layout';
 import Modal from '../../common/modal/Modal';
-import './Youtube.module.scss';
-import { useEffect, useState, useRef } from 'react';
-
-/**
+import './Youtube.scss';
+import { useEffect, useState } from 'react';
+/*
 	리액트는 단방향 데이터 바인딩
-	-부모에서 자식으로 데이터 전달 가능, 자식에서 부모로 데이터를 전달 불가
-	-props 전당, children 전달
+	- 부모에서 자식으로 데이터 전달가능하지만 자식에서 부모로는 데이터를 전달 불가
+	- props전달, children 전달
+	
+	리액트에서 자식 컴포넌트에서는 직접적으로 부모 컴포넌트의 state값 변경이 불가
+	- 해결방법 부모의 state변경함수를 자식 컴포넌트로 전달
+	- 자식컴포넌트에서는 전달받은 state변경함수로 간접적으로 부모 state값 변경가능
 
-	리액트에서 자식 컴포넌트에서는 직접적으로 부모컴포넌트의 state값 변경이 불가
-	- 해결방법 : 부모의 state변경함수를 자식컴포넌트로 전달
-	-자식 컴포넌트에서는 전달받은 state변경함수로 간접적으로 부모 state값 변경 가능
-
-	useRef로 jsx는 참조객체에 담을수 있음
-	컴포넌트는 useRef를 통한 참조객체를 담는 것이 불가
- */
+	useRef로 jsx는 참조 객체에 담을 수 있음
+*/
 
 export default function Youtube() {
-	const refEl = useRef(null);
 	const [Youtube, setYoutube] = useState([]);
 	const [IsModal, setIsModal] = useState(false);
 
 	const fetchYoutube = () => {
 		const api_key = process.env.REACT_APP_YOUTUBE_API;
 		const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-		//const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
-		const pid = 'PL2pfG9YtKxMJVwlAjAIV9TBF5-44WnQ-q';
+		const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
 		const num = 5;
 		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
 		fetch(resultURL)
 			.then((data) => data.json())
 			.then((json) => {
-				console.log(json);
+				console.log(json.items);
 				setYoutube(json.items);
 			});
 	};
-
 	useEffect(() => {
 		fetchYoutube();
 	}, []);
@@ -47,16 +42,16 @@ export default function Youtube() {
 				{Youtube.map((data, idx) => {
 					return (
 						<article key={idx}>
-							<h2 onClick={() => console.log(refEl)}>{data.snippet.title}</h2>
-							<p ref={refEl}>{data.snippet.description}</p>
-							<div className='pic' onClick={() => refEl.current.open()}>
+							<h2>{data.snippet.title}</h2>
+							<p>{data.snippet.description}</p>
+							<div className='pic' onClick={() => setIsModal(true)}>
 								<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
 							</div>
 						</article>
 					);
 				})}
 			</Layout>
-			<Modal setIsModal={setIsModal} ref={refEl}></Modal>
+			{IsModal && <Modal setIsModal={setIsModal}></Modal>}
 		</>
 	);
 }
