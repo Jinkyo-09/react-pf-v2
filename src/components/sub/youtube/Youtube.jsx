19428fb2b568a83c0ede496d8d68ee8e66f92d45
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-05 10:09:35 +0900

    설명 주석 및 유튜브 api연결

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,8 +1,18 @@
 import Layout from '../../common/layout/Layout';
-//import styles from './Department.module.scss';
-//import clsx from 'clsx';
+import './Youtube.module.scss';
+import { useEffect, useState } from 'react';
 
 export default function Youtube() {
+	const api_key = 'AIzaSyBzu-DiHzM7s-z6s-Elyy24zf2bBrf6ckk';
+	const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+	const pid = 'PL2pfG9YtKxMJVwlAjAIV9TBF5-44WnQ-q';
+	const num = 5;
+	const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+
+	useEffect(() => {
+		fetch();
+	}, []);
+
 	return (
 		<Layout title={'Youtube'}>
 			<p>유튜브 페이지입니다.</p>
