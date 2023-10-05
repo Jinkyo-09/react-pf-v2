a265866fd46b554d3235b799da07fbe0339c361f
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-05 10:34:24 +0900

    유튜브 데이터 출력 및 유튜브 api 배포파일에서 제외

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -3,19 +3,40 @@ import './Youtube.module.scss';
 import { useEffect, useState } from 'react';
 
 export default function Youtube() {
-	const api_key = 'AIzaSyBzu-DiHzM7s-z6s-Elyy24zf2bBrf6ckk';
-	const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-	const pid = 'PL2pfG9YtKxMJVwlAjAIV9TBF5-44WnQ-q';
-	const num = 5;
-	const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+	const [Youtube, setYoutube] = useState([]);
+
+	const fetchYoutube = () => {
+		const api_key = process.env.REACT_APP_YOUTUBE_API;
+		const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+		const pid = 'PL2pfG9YtKxMJVwlAjAIV9TBF5-44WnQ-q';
+		const num = 5;
+		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+
+		fetch(resultURL)
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json.items);
+				setYoutube(json.items);
+			});
+	};
 
 	useEffect(() => {
-		fetch();
+		fetchYoutube();
 	}, []);
 
 	return (
 		<Layout title={'Youtube'}>
-			<p>유튜브 페이지입니다.</p>
+			{Youtube.map((data, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{data.snippet.title}</h2>
+						<p>{data.snippet.description}</p>
+						<div className='pic'>
+							<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
