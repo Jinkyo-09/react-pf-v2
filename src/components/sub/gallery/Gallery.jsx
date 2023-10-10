0f050914cd44c50d9a03e188d6b10ffec568f94d
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-10 13:14:30 +0900

    검색창 기능 추가 및 프로필 이미지 에러 발생시 대체이미지 출력

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,9 +1,10 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
+	const refInput = useRef(null);
 	const [Pics, setPics] = useState([]);
 	const my_id = '199299808@N06';
 
@@ -12,7 +13,8 @@ export default function Gallery() {
 		const api_key = '2a1a0aebb34012a99c23e13b49175343';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
-		const num = 50;
+		const method_search = 'flickr.photos.search';
+		const num = 100;
 
 		if (opt.type === 'interest') {
 			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
@@ -26,22 +28,48 @@ export default function Gallery() {
 		if (opt.type === 'user') {
 			url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&user_id=${opt.id}`;
 		}
+		if (opt.type === 'search') {
+			url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tags}`;
+		}
 
 		const data = await fetch(url);
 		const json = await data.json();
 		console.log(json.photos.photo);
+		if (json.photos.photo.length === 0) {
+			return alert('검색어에 해당하는 이미지를 찾을수 없습니다.');
+		}
+
 		setPics(json.photos.photo);
 	};
 
 	useEffect(() => {
 		//type: 'interest' 인터레스트 방식 갤러리 호출
 		//type: 'user' 사용자 아이디 계정의 갤러리 호출
-		fetchData({ type: 'user', id: my_id });
+		//type: 'serch' 검색키워드로 갤러리 호출
+		//fetchData({ type: 'user', id: my_id });
 		//fetchData({ type: 'interest' });
+		fetchData({ type: 'user', id: my_id });
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
+			<form
+				onSubmit={(e) => {
+					//submit이벤트의 기본 서버 전송기능을 막아줌
+					e.preventDefault();
+					//문자열.trim() : 문자열앞뒤로 빈칸을 제거해서 정리
+					if (refInput.current.value.trim() === '') {
+						return alert('검색어를 입력하세요.');
+					}
+
+					fetchData({ type: 'search', tags: refInput.current.value });
+					refInput.current.value = '';
+				}}
+			>
+				<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
+				<button>검색</button>
+			</form>
+
 			<button onClick={() => fetchData({ type: 'user', id: my_id })}>My Gallery</button>
 			<button onClick={() => fetchData({ type: 'interest' })}>Interest Gallery</button>
 
@@ -64,7 +92,13 @@ export default function Gallery() {
 									<h2>{data.title}</h2>
 
 									<div className='profile'>
-										<img src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`} alt={data.owner} />
+										<img
+											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
+											alt={data.owner}
+											onError={(e) => {
+												e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
+											}}
+										/>
 										<span onClick={() => fetchData({ type: 'user', id: data.owner })}>{data.owner}</span>
 									</div>
 								</div>
