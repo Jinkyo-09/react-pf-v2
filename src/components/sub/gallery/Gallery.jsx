596ce8008ec757d277b530e37279cc4915eb28a5
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-10 09:49:58 +0900

    masonry ui 적용

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,6 +1,7 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import { useState, useEffect } from 'react';
+import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
@@ -21,25 +22,32 @@ export default function Gallery() {
 	return (
 		<Layout title={'Gallery'}>
 			<div className='picFrame'>
-				{Pics.map((data, idx) => {
-					return (
-						<article key={idx}>
-							<div className='inner'>
-								<img
-									className='pic'
-									src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
-									alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
-								/>
-								<h2>{data.title}</h2>
+				<Masonry
+					elementType={'div'} // masonry 컴포넌트가 변환될 태그명 지정
+					options={{ transitionDuration: '0.5s' }} // 박스 모션시 트랜지션 시간 설정
+					disableImagesLoaded={false} // true 이미지 로딩처리 안함
+					updateOnEachImageLoad={false} // true 각 이미지의 로딩 처리 안함
+				>
+					{Pics.map((data, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<img
+										className='pic'
+										src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
+										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
+									/>
+									<h2>{data.title}</h2>
 
-								<div className='profile'>
-									<img src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`} alt={data.owner} />
-									<span>{data.owner}</span>
+									<div className='profile'>
+										<img src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`} alt={data.owner} />
+										<span>{data.owner}</span>
+									</div>
 								</div>
-							</div>
-						</article>
-					);
-				})}
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
