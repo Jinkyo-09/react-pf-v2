1a2fb9a6afda8aaa23c7a149132496ca205d0b44
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-10 13:25:21 +0900

    Loader값에 따른 로딩바, 갤러리 프레임 분기처리

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -6,6 +6,7 @@ import Masonry from 'react-masonry-component';
 export default function Gallery() {
 	const refInput = useRef(null);
 	const [Pics, setPics] = useState([]);
+	const [Loader, setLoader] = useState(true);
 	const my_id = '199299808@N06';
 
 	const fetchData = async (opt) => {
@@ -53,60 +54,68 @@ export default function Gallery() {
 
 	return (
 		<Layout title={'Gallery'}>
-			<form
-				onSubmit={(e) => {
-					//submit이벤트의 기본 서버 전송기능을 막아줌
-					e.preventDefault();
-					//문자열.trim() : 문자열앞뒤로 빈칸을 제거해서 정리
-					if (refInput.current.value.trim() === '') {
-						return alert('검색어를 입력하세요.');
-					}
+			<div className='searchBox'>
+				<form
+					onSubmit={(e) => {
+						//submit이벤트의 기본 서버 전송기능을 막아줌
+						e.preventDefault();
+						//문자열.trim() : 문자열앞뒤로 빈칸을 제거해서 정리
+						if (refInput.current.value.trim() === '') {
+							return alert('검색어를 입력하세요.');
+						}
 
-					fetchData({ type: 'search', tags: refInput.current.value });
-					refInput.current.value = '';
-				}}
-			>
-				<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
-				<button>검색</button>
-			</form>
-
-			<button onClick={() => fetchData({ type: 'user', id: my_id })}>My Gallery</button>
-			<button onClick={() => fetchData({ type: 'interest' })}>Interest Gallery</button>
-
-			<div className='picFrame'>
-				<Masonry
-					elementType={'div'} // masonry 컴포넌트가 변환될 태그명 지정
-					options={{ transitionDuration: '0.5s' }} // 박스 모션시 트랜지션 시간 설정
-					disableImagesLoaded={false} // true 이미지 로딩처리 안함
-					updateOnEachImageLoad={false} // true 각 이미지의 로딩 처리 안함
+						fetchData({ type: 'search', tags: refInput.current.value });
+						refInput.current.value = '';
+					}}
 				>
-					{Pics.map((data, idx) => {
-						return (
-							<article key={idx}>
-								<div className='inner'>
-									<img
-										className='pic'
-										src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
-										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
-									/>
-									<h2>{data.title}</h2>
-
-									<div className='profile'>
-										<img
-											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
-											alt={data.owner}
-											onError={(e) => {
-												e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
-											}}
-										/>
-										<span onClick={() => fetchData({ type: 'user', id: data.owner })}>{data.owner}</span>
-									</div>
-								</div>
-							</article>
-						);
-					})}
-				</Masonry>
+					<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
+					<button>검색</button>
+				</form>
 			</div>
+
+			<div className='btnSet'>
+				<button onClick={() => fetchData({ type: 'user', id: my_id })}>My Gallery</button>
+				<button onClick={() => fetchData({ type: 'interest' })}>Interest Gallery</button>
+			</div>
+
+			{Loader ? (
+				<img className='loading' src={`${process.env.PUBLIC_URL}/img/loading.gif`} alt='loading' />
+			) : (
+				<div className='picFrame'>
+					<Masonry
+						elementType={'div'} // masonry 컴포넌트가 변환될 태그명 지정
+						options={{ transitionDuration: '0.5s' }} // 박스 모션시 트랜지션 시간 설정
+						disableImagesLoaded={false} // true 이미지 로딩처리 안함
+						updateOnEachImageLoad={false} // true 각 이미지의 로딩 처리 안함
+					>
+						{Pics.map((data, idx) => {
+							return (
+								<article key={idx}>
+									<div className='inner'>
+										<img
+											className='pic'
+											src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
+											alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
+										/>
+										<h2>{data.title}</h2>
+
+										<div className='profile'>
+											<img
+												src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
+												alt={data.owner}
+												onError={(e) => {
+													e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
+												}}
+											/>
+											<span onClick={() => fetchData({ type: 'user', id: data.owner })}>{data.owner}</span>
+										</div>
+									</div>
+								</article>
+							);
+						})}
+					</Masonry>
+				</div>
+			)}
 		</Layout>
 	);
 }
